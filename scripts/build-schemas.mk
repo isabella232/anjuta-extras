5633d6e9a33661ef89a6cef206dea88495a08a40
Nicolas Bruguier <nicolas.bruguier@supersonicimagine.fr>
2009-08-11 21:18:40 +0200

    bgo#590963 – Anjuta-extras make distcheck fail

--- scripts/build-schemas.mk
+++ scripts/build-schemas.mk
@@ -5,13 +5,16 @@ prefs_ui_schemas = $(prefs_ui_files:.ui=.schemas)
 prefs_ui_schemas_DATA = $(prefs_ui_schemas)
 
 %.schemas: %.ui
-	$(top_srcdir)/scripts/builder2schema.pl $(srcdir)/$(?) > $(@)
+	$(top_srcdir)/scripts/builder2schema.pl $< > $@
 
 if GCONF_SCHEMAS_INSTALL
-uninstall-local: $(prefs_glade_schemas) $(prefs_ui_schemas)
-	        for p in $(prefs_glade_schemas) ; do \
-	            GCONF_CONFIG_SOURCE=$(GCONF_SCHEMA_CONFIG_SOURCE) $(GCONFTOOL) --makefile-uninstall-rule $$p ; \
+install-data-local: $(prefs_ui_schemas)
+	        for p in $(prefs_ui_schemas) ; do \
+	            GCONF_CONFIG_SOURCE=$(GCONF_SCHEMA_CONFIG_SOURCE) $(GCONFTOOL) --makefile-install-rule $$p ; \
 	        done
+		@killall -1 gconfd-2 || true
+
+uninstall-local: $(prefs_ui_schemas)
 	        for p in $(prefs_ui_schemas) ; do \
 	            GCONF_CONFIG_SOURCE=$(GCONF_SCHEMA_CONFIG_SOURCE) $(GCONFTOOL) --makefile-uninstall-rule $$p ; \
 	        done
@@ -23,4 +26,4 @@ install-data-local:
 uninstall-local:
 endif
 
-CLEANFILES = $(prefs_glade_schemas)
+CLEANFILES = $(prefs_ui_schemas)
