9379a741aacf100606f79a1214ecc39d47045c7c
Carlos Jose de Almeida Pereira <carlao2005@gmail.com>
2010-08-28 11:32:32 +0200

    profiler: bgo #627970 - error in string (probably a typo)

--- plugins/profiler/plugin.c
+++ plugins/profiler/plugin.c
@@ -293,7 +293,7 @@ profiler_set_target (Profiler *profiler, const gchar *profile_target_uri)
 									   _("This target does not have any "
 									     "profiling data.\n\n"
 									     "Please ensure that the target is "
-									     "complied with profiling support (-p)"
+									     "compiled with profiling support (-p)"
 										 "and that it is run at least "
 										 "once."));
 		}
